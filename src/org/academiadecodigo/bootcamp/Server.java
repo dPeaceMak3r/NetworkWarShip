611b7923ecdb9689eefcdf777577f640ab697fdd
Luis Toste <luis.antonio.santos.toste@gmail.com>
2017-12-23 17:07:43 -0100

    Class server created

---
+++ src/org/academiadecodigo/bootcamp/Server.java
@@ -0,0 +1,17 @@
+package org.academiadecodigo.bootcamp;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    private final static int PORT_NUMBER = 8080;
+
+    public static void main(String[] args) {
+
+        ExecutorService players = Executors.newCachedThreadPool();
+
+
+    }
+
+}
