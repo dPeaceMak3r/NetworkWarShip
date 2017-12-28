76ed7573494d8503fec4a99bf12d77d13c563476
Luis Toste <luis.antonio.santos.toste@gmail.com>
2017-12-28 11:45:32 -0100

    Probalby working need to test some more

--- src/org/academiadecodigo/bootcamp/Server/ServerHelper.java
+++ src/org/academiadecodigo/bootcamp/Server/ServerHelper.java
@@ -1,8 +1,14 @@
 package org.academiadecodigo.bootcamp.Server;
 
+import java.io.*;
+
 public class ServerHelper implements Runnable {
 
     private Data[] playerData;
+    private int counter = 0;
+    private int miss = 0;
+    private String shipsDestroyed="";
+    private String shipsHit="";
 
     public ServerHelper(Data[] playerData){
 
@@ -10,8 +16,165 @@ public class ServerHelper implements Runnable {
 
     }
 
+    public void gameLogic(String[] shots){
+
+        miss = 0;
+        shipsDestroyed="";
+        shipsHit="";
+
+        for (int i=0;i<shots.length;i+=2){
+
+            int firstShot=Integer.parseInt(shots[i]);
+            int secondShot=Integer.parseInt(shots[i+1]);
+
+            if (playerData[counter].getGrid()[firstShot][secondShot] != null){
+
+                //Switch for the type of ships
+                switch (playerData[counter].getGrid()[firstShot][secondShot]){
+                    case "Ca":
+                        carrier.hit();
+                        shipsHit=shipsHit+" "+"Carrier hit";
+                        if(carrier.isDestroyed){
+                            shipsDestroyed=shipsDestroyed+" "+carrier.getMessage();
+                        }
+                        break;
+                    case "Ba":
+                        battleship.hit();
+                        shipsHit=shipsHit+" "+"Battleship hit";
+                        if(battleship.isDestroyed){
+                            shipsDestroyed=shipsDestroyed+" "+battleship.getMessage();
+                        }
+                        break;
+                    case "Cr":
+                        cruiser.hit();
+                        shipsHit=shipsHit+" "+"Cruiser hit";
+                        if(cruiser.isDestroyed){
+                            shipsDestroyed=shipsDestroyed+" "+cruiser.getMessage();
+                        }
+                        break;
+                    case "Su":
+                        submarine.hit();
+                        shipsHit=shipsHit+" "+"Submarine hit";
+                        if(submarine.isDestroyed){
+                            shipsDestroyed=shipsDestroyed+" "+submarine.getMessage();
+                        }
+                        break;
+                    case "De":
+                        destroyer.hit();
+                        shipsHit=shipsHit+" "+"Destroyer hit";
+                        if(destroyer.isDestroyed){
+                            shipsDestroyed=shipsDestroyed+" "+destroyer.getMessage();
+                        }
+                        break;
+
+                }
+
+                //playerData[counter].getGrid()[][]
+
+            }else{
+
+                miss++;
+
+            }
+
+        }
+
+
+
+    }
+
     @Override
     public void run() {
+
+        while (playerData[0].getClientSocket().isConnected() && playerData[1].getClientSocket().isConnected()){
+
+            try {
+
+                String[] shots;
+                BufferedReader in = new BufferedReader(new InputStreamReader(playerData[counter].getClientSocket().getInputStream()));
+                shots = in.readLine().split(" ");
+
+                if (shots.equals(null)){
+
+                    playerData[counter].getClientSocket().close();
+
+                }
+
+                changeCounter();
+                gameLogic(shots);
+
+                sendMessage(shots);
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    public void sendMessage(String [] shots){
+
+
+        try {
+
+            //Message to the player that got attacked
+            PrintWriter out = new PrintWriter(playerData[counter].getClientSocket().getOutputStream(), true);
+            if (miss < 3){
+
+                //This happens when one of the ships was destroyed
+                if(!shipsDestroyed.equals("")){
+                    out.println("Ships destroyed: "+shipsDestroyed);
+                }
+
+                //Message for the ships hit
+                out.println("Ships hit: "+shipsHit);
+
+            }
+            out.println("Number of ships hit: " + (3-miss));
+
+            //Send the shots so the opponent won't see them
+            out.println("/shots "+shots);
+
+
+            //Message to the player that attacked
+            changeCounter();
+            out = new PrintWriter(playerData[counter].getClientSocket().getOutputStream(), true);
+            if (miss < 3){
+
+                //This happens when one of the ships was destroyed
+                if(!shipsDestroyed.equals("")){
+                    out.println("Ships destroyed: "+shipsDestroyed);
+                }
+
+                //Message for the ships hit
+                out.println("Ships hit: "+shipsHit);
+
+            }
+
+            out.println("Number of ships hit: " + (3-miss));
+
+            //Last change counter
+            changeCounter();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void changeCounter(){
+
+        if (counter == 0){
+
+            counter = 1;
+
+        }else {
+
+            counter = 0;
+        }
 
     }
 
